9e39ff7c96a726c06896cbd15830d2759ff38ecf
Hayden Fields <hayden.fields@gmail.com>
2018-02-26 14:33:37 -0600

    Changed scheduling function to be more logically structured. Added some of the new constraints required by the client.

--- thirst-tea-cafe-employee-system/src/main/java/com/thirstteacafe/employees/SchedulingFunction.java
+++ thirst-tea-cafe-employee-system/src/main/java/com/thirstteacafe/employees/SchedulingFunction.java
@@ -12,129 +12,167 @@ public class SchedulingFunction
 {
     public static void main(String[] args)
     {
-        // determined that maxPerSlot is entirely useless
-        ArrayList<ArrayList<Integer>> s = schedule(
-                                                    "1 0 1\n" +
-                                                    "0 1 1\n" +
-                                                    "1 1 1\n",
-                                                        "1 2 1",
-                                                        "1 2 1"
-                                            );
-        for (ArrayList<Integer> row : s)
+        int[][] s = schedule(
+                    "1 0 1\n" +
+                    "0 1 1\n" +
+                    "1 0 1\n",
+                "2 1 3",
+                "3 3 3",
+                "1 1 1",
+                "1 1 1",
+                "1 1 1",
+                "1 1 1",
+                "1 1 1"
+                );
+        for (int[] e : s)
         {
-            for (Integer element : row)
-            {
-                System.out.printf("%d ", element);
-            }
+            for (int x : e)
+                System.out.printf("%d ", x);
             System.out.println();
         }
     }
-    
-    public static ArrayList<ArrayList<Integer>> schedule(String available, 
-                                                         String minPerSlot, 
-                                                         String maxPerSlot)
+    public static int[][] schedule(
+                                String available,
+                                String min      ,
+                                String max      ,
+                                String time     ,
+                                String admin    ,
+                                String canLift  ,
+                                String food     ,
+                                String drink    )
     {
-        return schedule(getMatrix(available), getRow(minPerSlot), getRow(maxPerSlot));
+        return schedule(convertMatrix(available),
+        
+        convert(min      ),        
+        convert(max      ),        
+        convert(time     ),        
+        convert(admin    ),        
+        convert(canLift  ),        
+        convert(food     ),        
+        convert(drink    )        
+                
+                
+        );
     }
     
-    // takes a 2d matrix where each value is 1 if person i is available for
-    // time slot j,
-    // the minimum and maximum number of people required for shift j
-    public static ArrayList<ArrayList<Integer>> schedule(
-                                    ArrayList<ArrayList<Integer>> available, 
-                                    ArrayList<Integer> minPerSlot, 
-                                    ArrayList<Integer> maxPerSlot)
+    public static int[][] schedule(int[][] available,
+                                    int[]   min,
+                                    int[]   max,
+                                    int[]   time,
+                                    int[]   admin,
+                                    int[]   canLift,
+                                    int[]   food,
+                                    int[]   drink)
     {
-        // will hold the resulting scheduling at the end
-        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        // assumes the input is rectangular
+        final int numberOfEmployees = available.length;
+        final int numberOfTimeSlots = available[0].length;
+        final int C = 128; // sufficiently large constants for hour constraint
         
-        // store for all variables and constraints
         Store store = new Store();
         
-        final int numberOfEmployees = available.size();
-        final int numberOfTimeSlots = available.get(0).size();
-
-        // a schedule simply indicates when person i works on time slot j
-        BooleanVar[][] schedule = new BooleanVar[numberOfEmployees]
-                                                [numberOfTimeSlots];
-        for (int i = 0; i < numberOfEmployees; i++)
-        {
-            for (int j = 0; j < numberOfTimeSlots; j++)
-            {   
-                schedule[i][j] = 
-                        new BooleanVar(store, String.format("P_%d,%d", i,j));
-            }
-        }
+     
+        // work matrix
+        IntVar[][] work = new IntVar[numberOfEmployees][numberOfTimeSlots];
         
-        // impose that employees cannot work when they are not available
+        // transposed version of the work matrix
+        IntVar[][] workT = new IntVar[numberOfTimeSlots][numberOfEmployees];
+        
         for (int i = 0; i < numberOfEmployees; i++)
-        {
-            for (int j = 0 ; j < numberOfTimeSlots; j++)
+            for (int j = 0; j < numberOfEmployees; j++)
             {
-                if (available.get(i).get(j) == 0)
-                {
-                    store.impose(new XeqC(schedule[i][j],0));
-                }
+                work[i][j] = workT[j][i]= new IntVar(store, String.format("work_%d,%d",i,j),0,1);
+                 
             }
-        }
         
-        // s_j tracks how many employees are working shift j
-        for (int j = 0; j < numberOfTimeSlots; j++)
-        {
-            BooleanVar[] f = new BooleanVar[numberOfEmployees];
-            for (int i = 0; i < numberOfEmployees; i++)
-                f[i] = schedule[i][j];
-            // the sum of everyone working shift j is how many people on shift j
-            store.impose(new SumBool(store,
-                    f,
-                    "==",
-                    new IntVar(store,String.format("S_%d",j),
-                               minPerSlot.get(j),maxPerSlot.get(j))));
-        }
         
-        // TODO: restructure this so it makes sense
+        // define problem constraints
         
-        // looks like all variables that are to be optimized must be passed
-        // in as the second parameter to select choice point
-        IntVar[] f = new IntVar[numberOfEmployees * 2 * numberOfTimeSlots];
+        // If the employee is not available for the time slot then the employee 
+        // cannot work the time slot
         for (int i = 0; i < numberOfEmployees; i++)
-        {
             for (int j = 0; j < numberOfTimeSlots; j++)
-            {
-                f[i * numberOfEmployees + j] = schedule[i][j];
-            }
-        }
+                if (available[i][j] == 0)
+                    store.impose(new XeqC(work[i][j],0));
+        
+        // Must have no less than minimum number of employees and no more than
+        // maximum number of employees' for slot.
         for (int j = 0; j < numberOfEmployees; j++)
         {
-            
-            {
-                f[numberOfEmployees * numberOfTimeSlots + j] = 
-                        (IntVar)store.variablesHashMap.get(String.format("S_%d",j));
-            }
+            // is there a way to use constants here instead of constant intVars?
+            store.impose(new SumBool(store,workT[j],">=",
+                    new IntVar(store,String.format("min_%d",j),min[j],min[j])));
+            store.impose(new SumBool(store,workT[j],"<=",
+                    new IntVar(store,String.format("max_%d",j),max[j],max[j])));
         }
         
-        Search<IntVar> label = new DepthFirstSearch<IntVar>();
+        // for each time slot must have 1 admin
+        for (int j = 0; j < numberOfTimeSlots; j++)
+            store.impose(new LinearInt(store,workT[j],admin,">=", 1));
+        //for each time slot must have 1 lifter
+        for (int j = 0; j < numberOfTimeSlots; j++)
+            store.impose(new LinearInt(store,workT[j],canLift,">=", 1));
+        // for each time slot must have 1 food maker
+        for (int j = 0; j < numberOfTimeSlots; j++)
+            store.impose(new LinearInt(store,workT[j],food,">=", 1));
+        // for each time slot must have 1 drink maker
+        for (int j = 0; j < numberOfTimeSlots; j++)
+            store.impose(new LinearInt(store,workT[j],drink,">=", 1));
         
-        // stop search from printing to standard out
-        label.setPrintInfo(false);
-        SelectChoicePoint<IntVar> select = 
-                    new InputOrderSelect<>(store,f, new IndomainMin<IntVar>());
-        
-        // computes the schedule
-        label.labeling(store, select);
-        
+        // for each employee they can work no more than 40 hours unless they
+        // are an admin
         for (int i = 0; i < numberOfEmployees; i++)
-        {
-            result.add(new ArrayList<>());
-            for (int j = 0; j < numberOfTimeSlots; j++)
-            {
-                result.get(i).add(schedule[i][j].value());
-            }
-        }
+            store.impose(new LinearInt(store,work[i],time,"<=", 40 + admin[i] * C));
         
+        
+        // compute oldSchedule
+        Search<IntVar> label = new DepthFirstSearch<IntVar>();
+        label.setPrintInfo(false);
+        
+        SelectChoicePoint<IntVar> select = 
+                    new InputOrderSelect<>(store,flatten(work), new IndomainMin<IntVar>());
+        // TODO: actually do something with this feasbility value
+        boolean feasible = label.labeling(store, select);
+        
+        int[][] result = new int[numberOfEmployees][numberOfTimeSlots];
+        for (int i = 0; i < numberOfEmployees; i++)
+            for (int j = 0; j < numberOfTimeSlots; j++)
+                result[i][j] = work[i][j].value();
         return result;
     }
     
+    public static IntVar[] flatten(IntVar[][] variables)
+    {
+        // assume input is a rectangular matrix of variables
+        IntVar[] flatList = new IntVar[variables.length * variables[0].length];
+        for (int i = 0; i < variables.length; i++)
+            for (int j = 0; j < variables.length; j++)
+                flatList[i * variables[0].length + j] = variables[i][j];
+        return flatList;
+    }
+    public static int[] convert(String input)
+    {
+        return convert(getRow(input));
+    }
+    public static int[] convert(ArrayList<Integer> list)
+    {
+        // since java is weird we:
+        // convert the list to a Stream
+        // the stream to a IntStream with a lambda to unbox the Integer Objects
+        // IntStream has a toArray function that converts it to a int[]
+        return list.stream().mapToInt(i -> i).toArray();
+    }
+    public static int[][] convertMatrix(String input)
+    {
+        return convertMatrix(getMatrix(input));
+    }
+    public static int[][] convertMatrix(ArrayList<ArrayList<Integer>> list)
+    {
+        int[][] acc = new int[list.size()][list.get(0).size()];
+        for (int i = 0; i < list.size(); i++)
+            acc[i] = convert(list.get(i));
+        return acc;
+    }
     public static ArrayList<Integer> getRow(String input)
     {
         ArrayList<Integer> row = new ArrayList<Integer>();
