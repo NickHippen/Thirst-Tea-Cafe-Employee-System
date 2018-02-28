e358276221841742211653a791dc51cf29906738
Nick Hippen <nhippen@unomaha.edu>
2018-02-28 16:20:29 -0600

    Added timeslot service tests

---
+++ thirst-tea-cafe-employee-system/src/test/java/com/thirstteacafe/employees/schedule/TimeslotServiceTest.java
@@ -0,0 +1,34 @@
+package com.thirstteacafe.employees.schedule;
+
+import static org.junit.Assert.assertEquals;
+
+import java.time.LocalTime;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TimeslotServiceTest {
+
+	@Autowired
+	private TimeslotService timeslotService;
+	
+	@Before
+	public void before() {
+	}
+	
+	@Test
+	public void testConvertLocalTime() {
+		assertEquals(0, timeslotService.convertLocalTime(LocalTime.of(0, 0)));
+		assertEquals(47, timeslotService.convertLocalTime(LocalTime.of(23, 59, 59)));
+		assertEquals(24, timeslotService.convertLocalTime(LocalTime.of(12, 0)));
+		assertEquals(13, timeslotService.convertLocalTime(LocalTime.of(6, 30)));
+		assertEquals(13, timeslotService.convertLocalTime(LocalTime.of(6, 59)));
+	}
+	
+}
