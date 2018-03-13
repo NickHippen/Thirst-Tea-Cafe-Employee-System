a527a90069fb00fcb8a5595307568034c67369df
Nick Hippen <nhippen@unomaha.edu>
2018-03-13 13:46:37 -0500

    Added tt-schedule component

--- thirst-tea-cafe-employee-system-ui/src/common/components/index.js
+++ thirst-tea-cafe-employee-system-ui/src/common/components/index.js
@@ -1,5 +1,6 @@
+import ttSchedule from './tt-schedule/tt-schedule.component';
 
 export default angular.module('tt-employee.common.components', [
-  
 ])
+.component('ttSchedule', ttSchedule)
 .name;
---
+++ thirst-tea-cafe-employee-system-ui/src/common/components/tt-schedule/tt-schedule.component.js
@@ -0,0 +1,7 @@
+import ttScheduleTemplate from './tt-schedule.html';
+import ttScheduleCtrl from './tt-schedule.controller';
+
+export default {
+  template: ttScheduleTemplate,
+  controller: ttScheduleCtrl
+};
