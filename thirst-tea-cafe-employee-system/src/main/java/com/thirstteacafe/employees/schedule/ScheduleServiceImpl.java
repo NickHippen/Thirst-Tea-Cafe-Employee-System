52f3a1ed2a393c88200e5b42678bae1fbb7da7f4
Hayden Fields <hayden.fields@gmail.com>
2018-03-26 11:51:10 -0500

    Found the cause of the performance issues . It was the min max hours per employee constraint.

--- thirst-tea-cafe-employee-system/src/main/java/com/thirstteacafe/employees/schedule/ScheduleServiceImpl.java
+++ thirst-tea-cafe-employee-system/src/main/java/com/thirstteacafe/employees/schedule/ScheduleServiceImpl.java
@@ -20,9 +20,11 @@ import com.thirstteacafe.employees.dto.Employee;
 import com.thirstteacafe.employees.dto.ScheduleResult;
 import com.thirstteacafe.employees.dto.Shift;
 import com.thirstteacafe.employees.util.MatrixUtil;
+import java.util.ArrayList;
 import org.jacop.constraints.Constraint;
 import org.jacop.constraints.Or;
 import org.jacop.constraints.PrimitiveConstraint;
+import org.jacop.constraints.XplusYgtC;
 
 @Service
 public class ScheduleServiceImpl implements ScheduleService {
@@ -75,6 +77,7 @@ public class ScheduleServiceImpl implements ScheduleService {
 				minEmployees, maxEmployees, time, adminOnly);
 	}
 	
+        
 	@Override
 	public ScheduleResult schedule(String available,
 			String admin, String canLift, String food, String drink, String minHours, String maxHours,
@@ -105,27 +108,32 @@ public class ScheduleServiceImpl implements ScheduleService {
         // transposed version of the work matrix
         IntVar[][] workT = new IntVar[numberOfTimeSlots][numberOfEmployees];
         
-//        // foodDuty_i,j is 1 if employee i is assigned to food duty on time slot j
-//        IntVar[][] foodDuty = new IntVar[numberOfEmployees][numberOfTimeSlots];
-//        IntVar[][] foodDutyT = new IntVar[numberOfTimeSlots][numberOfEmployees];
-//        // drinkDuty_i,j is 1 if employee i is assigned to drink duty on time slot j
-//        IntVar[][] drinkDuty = new IntVar[numberOfEmployees][numberOfTimeSlots];
-//        IntVar[][] drinkDutyT = new IntVar[numberOfTimeSlots][numberOfEmployees];
-//        for (int i = 0; i < numberOfEmployees; i++)
-//        {
-//            for (int j = 0; j < numberOfTimeSlots; j++)
-//            {
-//                foodDuty[i][j] = foodDutyT[j][i] = new IntVar(store,String.format("food_%d,%d", i,j),0,food[i]);
-//                drinkDuty[i][j] = drinkDutyT[j][i] = new IntVar(store,String.format("drink_%d,%d", i,j),0,drink[i]);
-//            }
-//        }
-        
+        // work[i][j] is 1 if employee i works time slot j and 0 otherwise
         for (int i = 0; i < numberOfEmployees; i++)
             for (int j = 0; j < numberOfTimeSlots; j++)
                 work[i][j] = workT[j][i] = new IntVar(store, String.format("work_%d,%d",i,j),0,1);
-        
+
         // define problem constraints
         
+        // There must be at least one viable pair that is working,
+        // additionally constrains that there must be at least 1 food maker
+        // and at least 1 drink maker
+        for (int j = 0; j < numberOfTimeSlots; j++)
+        {
+            ArrayList<PrimitiveConstraint> viablePairs = new ArrayList<>();
+            for (int i1 = 0 ; i1 < numberOfEmployees; i1++)
+            {
+                for (int i2 = i1 + 1; i2 < numberOfEmployees; i2++)
+                {
+                    // if it is possible for the pair to be viable
+                    if (food[i1] + drink[i2] >= 2 || drink[i1] + food[i2] >= 2)
+                    {
+                        viablePairs.add(new XplusYgtC(work[i1][j],work[i2][j], 1));
+                    }
+                }
+            }
+            store.impose(new Or(viablePairs));
+        }        
         // If the employee is not available for the time slot then the employee 
         // cannot work the time slot
         for (int i = 0; i < numberOfEmployees; i++)
@@ -150,66 +158,14 @@ public class ScheduleServiceImpl implements ScheduleService {
         //for each time slot must have 1 lifter
         for (int j = 0; j < numberOfTimeSlots; j++)
             store.impose(new LinearInt(store,workT[j],canLift,">=", 1));
-        // for each time slot must have 1 food maker (possibly redundant)
-        for (int j = 0; j < numberOfTimeSlots; j++)
-            store.impose(new LinearInt(store,workT[j],food,">=", 1));
-        //for each time slot must have 1 drink maker (possibly redundant)
-        for (int j = 0; j < numberOfTimeSlots; j++)
-            store.impose(new LinearInt(store,workT[j],drink,">=", 1));
         
-        
-        
-//        //for each time slot must have 1 food maker
-//        for (int j = 0; j < numberOfTimeSlots; j++)
-//            store.impose(new SumBool(store,foodDutyT[j],">=",ONE));
-//        //for each time slot must have 1 drink maker
-//        for (int j = 0; j < numberOfTimeSlots; j++)
-//            store.impose(new SumBool(store,drinkDutyT[j],">=",ONE));    
-        
-//        // for each employee on each shift if they are working they can make 
-//        // food xor make drinks
-//        for (int i = 0; i < numberOfEmployees; i++)
-//            for (int j = 0; j < numberOfTimeSlots; j++)
-//                store.impose(new XplusYlteqZ(foodDuty[i][j],drinkDuty[i][j],work[i][j]));
-//  
-        
-        for (int j = 0; j < numberOfTimeSlots; j++)
-        {
-            PrimitiveConstraint[] pairs = new PrimitiveConstraint[numberOfEmployees * (numberOfEmployees - 1) / 2];
-            int c = 0;
-            for (int i1 = 0; i1 < numberOfEmployees; i1++)
-            {
-                for (int i2 = i1 + 1; i2 < numberOfEmployees; i2++)
-                {
-                    pairs[c++] = new LinearInt(store,
-                            new IntVar[]{work[i1][j],work[i2][j]},new int[]{1,1},
-                            ">=", 
-                            4 - food[i1] - food[i2]);
-                }
-            }
-            store.impose(new Or(pairs));
-        }
-        for (int j = 0; j < numberOfTimeSlots; j++)
-        {
-            PrimitiveConstraint[] pairs = new PrimitiveConstraint[numberOfEmployees * (numberOfEmployees - 1) / 2];
-            int c = 0;
-            for (int i1 = 0; i1 < numberOfEmployees; i1++)
-            {
-                for (int i2 = i1 + 1; i2 < numberOfEmployees; i2++)
-                {
-                    pairs[c++] = new LinearInt(store,
-                            new IntVar[]{work[i1][j],work[i2][j]},new int[]{1,1},
-                            ">=", 
-                            4 - drink[i1] - drink[i2]);
-                }
-            }
-            store.impose(new Or(pairs));
-        }
+        // this runs real slow for some reason
         // Each employee must work between their minimum and maximum hours
-        for (int i = 0; i < numberOfEmployees; i++)
-            store.impose(new LinearInt(store,work[i],time,">=", minHours[i]));
-        for (int i = 0; i < numberOfEmployees; i++)
-            store.impose(new LinearInt(store,work[i],time,"<=", maxHours[i]));    
+//        for (int i = 0; i < numberOfEmployees; i++)
+//            store.impose(new LinearInt(store,work[i],time,">=", minHours[i]));
+//        for (int i = 0; i < numberOfEmployees; i++)
+//            store.impose(new LinearInt(store,work[i],time,"<=", maxHours[i]));    
+//        
         // for admin only shifts if employee is not an admin then they are
         // not available
         for (int j = 0; j < numberOfTimeSlots; j++)
@@ -222,10 +178,9 @@ public class ScheduleServiceImpl implements ScheduleService {
         Search<IntVar> label = new DepthFirstSearch<IntVar>();
         //label.setPrintInfo(false);
         
-        
         SelectChoicePoint<IntVar> select = 
                     new InputOrderSelect<>(store,matrixUtil.flatten(work), new IndomainMin<IntVar>());
-        // TODO: actually do something with this feasbility value
+        
         boolean feasible = label.labeling(store, select);
         if (!feasible) 
             System.err.print("not feasible");
@@ -239,5 +194,4 @@ public class ScheduleServiceImpl implements ScheduleService {
         }
         return new ScheduleResult(result, feasible);
 	}
-
 }
