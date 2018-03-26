e64bbbf68c1e6d6771c5d9a511f44987344347c6
Nick Hippen <nhippen@unomaha.edu>
2018-03-26 18:45:16 -0500

    Added jsdoc tooling & added documentation to timeslot service

--- thirst-tea-cafe-employee-system-ui/gulp_tasks/misc.js
+++ thirst-tea-cafe-employee-system-ui/gulp_tasks/misc.js
@@ -3,11 +3,13 @@ const path = require('path');
 const gulp = require('gulp');
 const del = require('del');
 const filter = require('gulp-filter');
+const jsdoc = require('gulp-jsdoc3');
 
 const conf = require('../conf/gulp.conf');
 
 gulp.task('clean', clean);
 gulp.task('other', other);
+gulp.task('doc', doc);
 
 function clean() {
   return del([conf.paths.dist, conf.paths.tmp]);
@@ -22,4 +24,9 @@ function other() {
   ])
     .pipe(fileFilter)
     .pipe(gulp.dest(conf.paths.dist));
+}
+
+function doc(cb) {
+  gulp.src(['./src/**/*.js'], {read: false})
+        .pipe(jsdoc(cb));
 }
