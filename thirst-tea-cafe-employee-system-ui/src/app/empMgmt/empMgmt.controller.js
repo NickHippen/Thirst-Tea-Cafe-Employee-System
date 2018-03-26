1762dcdba5fcbcd13c18f95f13d5a3ae30d9605a
Nick Hippen <nhippen973@gmail.com>
2018-03-25 21:01:50 -0500

    Added initial employee availability view

--- thirst-tea-cafe-employee-system-ui/src/app/empMgmt/empMgmt.controller.js
+++ thirst-tea-cafe-employee-system-ui/src/app/empMgmt/empMgmt.controller.js
@@ -1,8 +1,31 @@
 export default class {
 
-  constructor($log, EmpMgmtService) {
+  constructor($log, EmpMgmtService, DAY_OF_WEEK) {
     'ngInject';
-    angular.extend(this, {$log, EmpMgmtService});
+    angular.extend(this, {$log, EmpMgmtService, DAY_OF_WEEK});
+
+    this.employees = [
+      {
+        employeeId: 1,
+        name: 'Nick Hippen'
+      },
+      {
+        employeeId: 2,
+        name: 'Vincent Nguyen'
+      },
+      {
+        employeeId: 3,
+        name: 'Mitch Huston'
+      },
+      {
+        employeeId: 4,
+        name: 'Hayden Fields'
+      }
+    ];
+  }
+
+  selectEmployee(employee) {
+    this.selectedEmployee = employee;
   }
 
 }
--- thirst-tea-cafe-employee-system-ui/src/app/empMgmt/index.js
+++ thirst-tea-cafe-employee-system-ui/src/app/empMgmt/index.js
@@ -2,6 +2,8 @@ import empMgmtConfig from './empMgmt.config';
 import empMgmt from './empMgmt.component';
 import EmpMgmtService from './empMgmt.service';
 
+import './empMgmt.scss';
+
 export default angular.module('tt-employee.empMgmt', [])
 .config(empMgmtConfig)
 .component('empMgmt', empMgmt)
