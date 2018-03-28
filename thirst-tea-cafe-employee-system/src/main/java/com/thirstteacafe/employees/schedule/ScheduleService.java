0d646dbedeb3b30ad5bf8055c23511606d787ae9
Hayden Fields <hayden.fields@gmail.com>
2018-03-27 22:15:49 -0500

    Added constraints for employees only working once per day.

--- thirst-tea-cafe-employee-system/src/main/java/com/thirstteacafe/employees/schedule/ScheduleService.java
+++ thirst-tea-cafe-employee-system/src/main/java/com/thirstteacafe/employees/schedule/ScheduleService.java
@@ -11,6 +11,6 @@ public interface ScheduleService {
 	ScheduleResult scheduleEmployees(List<Employee> employees, List<Shift> shifts);
 	
 	ScheduleResult schedule(String available, String admin, String canLift, String food, String drink, String minHours, String maxHours,
-                String minEmployees, String maxEmployees, String time, String adminOnly);
+                String minEmployees, String maxEmployees, String time, String adminOnly, String days);
 
 }
