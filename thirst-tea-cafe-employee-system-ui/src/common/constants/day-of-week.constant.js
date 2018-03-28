e542779d25ef2605910567333ccbbce1ce1a9d29
Nick Hippen <nhippen@unomaha.edu>
2018-03-28 18:47:25 -0500

    Added more data to week constant

--- thirst-tea-cafe-employee-system-ui/src/common/constants/day-of-week.constant.js
+++ thirst-tea-cafe-employee-system-ui/src/common/constants/day-of-week.constant.js
@@ -3,11 +3,32 @@
  * @module DAY_OF_WEEK
  */
 export default {
-  MONDAY: 'Monday',
-  TUESDAY: 'Tuesday',
-  WEDNESDAY: 'Wednesday',
-  THURSDAY: 'Thursday',
-  FRIDAY: 'Friday',
-  SATURDAY: 'Saturday',
-  SUNDAY: 'Sunday'
+  MONDAY: {
+    offset: 0,
+    name: 'Monday'
+  },
+  TUESDAY: {
+    offset: 1,
+    name: 'Tuesday'
+  },
+  WEDNESDAY: {
+    offset: 2,
+    name: 'Wednesday'
+  },
+  THURSDAY: {
+    offset: 3,
+    name: 'Thursday'
+  },
+  FRIDAY: {
+    offset: 4,
+    name: 'Friday'
+  },
+  SATURDAY: {
+    offset: 5,
+    name: 'Saturday'
+  },
+  SUNDAY: {
+    offset: 6,
+    name: 'Sunday'
+  }
 };
