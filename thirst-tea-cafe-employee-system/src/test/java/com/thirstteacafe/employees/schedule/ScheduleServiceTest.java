6c26c4f918424c68725dc97ad1a9c94c583127a0
Nick Hippen <nhippen@unomaha.edu>
2018-04-18 18:27:49 -0500

    Added proper real data test

--- thirst-tea-cafe-employee-system/src/test/java/com/thirstteacafe/employees/schedule/ScheduleServiceTest.java
+++ thirst-tea-cafe-employee-system/src/test/java/com/thirstteacafe/employees/schedule/ScheduleServiceTest.java
@@ -188,6 +188,10 @@ public class ScheduleServiceTest {
 		jp.addAvailability(DayOfWeek.WEDNESDAY, new DailyAvailability(34, 44));
 		jp.addAvailability(DayOfWeek.SATURDAY, new DailyAvailability(22, 34));
 		jp.addAvailability(DayOfWeek.SUNDAY, new DailyAvailability(22, 42));
+		jp.setMinHours(5);
+		jp.setMaxHours(11);
+		jp.setDrinkMaker(true);
+		jp.setCanLift(true);
 		
 		Employee ef = new Employee();
 		ef.addAvailability(DayOfWeek.MONDAY, new DailyAvailability(22, 44));
@@ -197,6 +201,9 @@ public class ScheduleServiceTest {
 		ef.addAvailability(DayOfWeek.FRIDAY, new DailyAvailability(22, 46));
 		ef.addAvailability(DayOfWeek.SATURDAY, new DailyAvailability(22, 46));
 		ef.addAvailability(DayOfWeek.SUNDAY, new DailyAvailability(22, 42));
+		ef.setMinHours(50);
+		ef.setMaxHours(60);
+		ef.setDrinkMaker(true);
 		
 		Employee bl = new Employee();
 		bl.addAvailability(DayOfWeek.MONDAY, new DailyAvailability(25, 44));
@@ -205,6 +212,10 @@ public class ScheduleServiceTest {
 		bl.addAvailability(DayOfWeek.FRIDAY, new DailyAvailability(22, 46));
 		bl.addAvailability(DayOfWeek.SATURDAY, new DailyAvailability(22, 46));
 		bl.addAvailability(DayOfWeek.SUNDAY, new DailyAvailability(22, 42));
+		bl.setMinHours(5);
+		bl.setMaxHours(22);
+		bl.setDrinkMaker(true);
+		bl.setCanLift(true);
 		
 		Employee js = new Employee();
 		js.addAvailability(DayOfWeek.MONDAY, new DailyAvailability(22, 34));
@@ -212,6 +223,10 @@ public class ScheduleServiceTest {
 		js.addAvailability(DayOfWeek.WEDNESDAY, new DailyAvailability(22, 44));
 		js.addAvailability(DayOfWeek.THURSDAY, new DailyAvailability(22, 44));
 		js.addAvailability(DayOfWeek.THURSDAY, new DailyAvailability(22, 35));
+		js.setMinHours(5);
+		js.setMaxHours(30);
+		js.setDrinkMaker(true);
+		js.setCanLift(true);
 		
 		Employee rs = new Employee();
 		rs.addAvailability(DayOfWeek.MONDAY, new DailyAvailability(28, 44));
@@ -219,6 +234,8 @@ public class ScheduleServiceTest {
 		rs.addAvailability(DayOfWeek.WEDNESDAY, new DailyAvailability(28, 44));
 		rs.addAvailability(DayOfWeek.THURSDAY, new DailyAvailability(22, 44));
 		rs.addAvailability(DayOfWeek.SATURDAY, new DailyAvailability(22, 46));
+		rs.setMinHours(0);
+		rs.setMaxHours(15);
 		
 		Employee tp = new Employee();
 		tp.addAvailability(DayOfWeek.MONDAY, new DailyAvailability(22, 44));
@@ -228,6 +245,11 @@ public class ScheduleServiceTest {
 		tp.addAvailability(DayOfWeek.FRIDAY, new DailyAvailability(22, 46));
 		tp.addAvailability(DayOfWeek.SATURDAY, new DailyAvailability(22, 46));
 		tp.addAvailability(DayOfWeek.SUNDAY, new DailyAvailability(22, 42));
+		tp.setMinHours(0);
+		tp.setMaxHours(999);
+		tp.setFoodMaker(true);
+		tp.setDrinkMaker(true);
+		tp.setAdmin(true);
 		
 		Employee ww = new Employee();
 		ww.addAvailability(DayOfWeek.MONDAY, new DailyAvailability(22, 44));
@@ -237,6 +259,10 @@ public class ScheduleServiceTest {
 		ww.addAvailability(DayOfWeek.FRIDAY, new DailyAvailability(22, 46));
 		ww.addAvailability(DayOfWeek.SATURDAY, new DailyAvailability(22, 46));
 		ww.addAvailability(DayOfWeek.SUNDAY, new DailyAvailability(22, 42));
+		ww.setMinHours(0);
+		ww.setMaxHours(999);
+		ww.setFoodMaker(true);
+		ww.setDrinkMaker(true);
 		
 		Employee id = new Employee();
 		id.addAvailability(DayOfWeek.MONDAY, new DailyAvailability(22, 44));
@@ -246,7 +272,43 @@ public class ScheduleServiceTest {
 		id.addAvailability(DayOfWeek.FRIDAY, new DailyAvailability(22, 46));
 		id.addAvailability(DayOfWeek.SATURDAY, new DailyAvailability(22, 46));
 		id.addAvailability(DayOfWeek.SUNDAY, new DailyAvailability(22, 42));
-//		scheduleService.scheduleEmployees(employees, shifts); TODO
+		id.setMinHours(0);
+		id.setMaxHours(999);
+		id.setFoodMaker(true);
+		id.setDrinkMaker(true);
+		
+		List<Employee> employees = Arrays.asList(jp, ef, bl, js, rs, tp, ww, id);
+		
+		Shift m1 = new Shift(DayOfWeek.MONDAY, 22, 44, 1, 1, true);
+		Shift m2 = new Shift(DayOfWeek.MONDAY, 22, 34, 1, 1, false);
+		Shift m3 = new Shift(DayOfWeek.MONDAY, 34, 44, 2, 2, false);
+		
+		Shift t1 = new Shift(DayOfWeek.TUESDAY, 22, 44, 1, 1, true);
+		Shift t2 = new Shift(DayOfWeek.TUESDAY, 22, 44, 1, 1, false);
+		Shift t3 = new Shift(DayOfWeek.TUESDAY, 22, 34, 1, 1, false);
+		Shift t4 = new Shift(DayOfWeek.TUESDAY, 34, 44, 1, 1, false);
+		
+		Shift w1 = new Shift(DayOfWeek.WEDNESDAY, 22, 44, 2, 2, true);
+		Shift w2 = new Shift(DayOfWeek.WEDNESDAY, 22, 44, 1, 1, false);
+		
+		Shift th1 = new Shift(DayOfWeek.THURSDAY, 22, 44, 1, 1, true);
+		Shift th2 = new Shift(DayOfWeek.THURSDAY, 22, 44, 1, 1, false);
+		
+		Shift f1 = new Shift(DayOfWeek.FRIDAY, 22, 46, 1, 1, true);
+		Shift f2 = new Shift(DayOfWeek.FRIDAY, 22, 46, 1, 1, false);
+		Shift f3 = new Shift(DayOfWeek.FRIDAY, 34, 46, 1, 1, false);
+		
+		Shift sa1 = new Shift(DayOfWeek.SATURDAY, 22, 46, 1, 1, true);
+		Shift sa2 = new Shift(DayOfWeek.SATURDAY, 22, 34, 1, 1, false);
+		Shift sa3 = new Shift(DayOfWeek.SATURDAY, 34, 46, 2, 2, false);
+		
+		Shift su1 = new Shift(DayOfWeek.SUNDAY, 22, 42, 1, 1, true);
+		Shift su2 = new Shift(DayOfWeek.SUNDAY, 22, 42, 1, 1, false);
+		Shift su3 = new Shift(DayOfWeek.SUNDAY, 32, 42, 1, 1, false);
+		
+		List<Shift> shifts = Arrays.asList(m1, m2, m3, t1, t2, t3, t4, w1, w2, th1, th2, f1, f2, f3, sa1, sa2, sa3, su1, su2, su3);
+		
+		scheduleService.scheduleEmployees(employees, shifts);
 	}
 
 	// // M) Monday (1100-1230-1400-1700-1900-2000) 5
