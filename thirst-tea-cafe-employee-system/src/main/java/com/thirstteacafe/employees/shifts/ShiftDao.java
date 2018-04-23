0bdad68a414c52d4cb8e35c0fceea1a3c870046a
vlnguyen <vlnguyen@unomaha.edu>
2018-04-23 16:13:30 -0500

    ShiftService: get all shifts

--- thirst-tea-cafe-employee-system/src/main/java/com/thirstteacafe/employees/shifts/ShiftDao.java
+++ thirst-tea-cafe-employee-system/src/main/java/com/thirstteacafe/employees/shifts/ShiftDao.java
@@ -1,10 +1,13 @@
 package com.thirstteacafe.employees.shifts;
 
+import java.util.List;
 public interface ShiftDao {
 
 	void createShift(ShiftData shift);
 	
 	ShiftData getShiftByID(int shiftID);
+
+	List<ShiftData> getAllShifts();
 
 	void deleteShiftByID(int shiftID);
 	
