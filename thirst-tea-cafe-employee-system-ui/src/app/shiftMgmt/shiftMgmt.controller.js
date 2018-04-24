9300d9c661f2db6ba19402d3c970c88946a2b4f3
vlnguyen <vlnguyen@unomaha.edu>
2018-04-24 18:05:19 -0500

    Viewing shifts connected to backend

--- thirst-tea-cafe-employee-system-ui/src/app/shiftMgmt/shiftMgmt.controller.js
+++ thirst-tea-cafe-employee-system-ui/src/app/shiftMgmt/shiftMgmt.controller.js
@@ -1,59 +1,27 @@
 export default class {
 
-  constructor($log, ShiftService, DAY_OF_WEEK) {
+  constructor($log, ShiftService, TimeslotService, LoadingService, DAY_OF_WEEK) {
     'ngInject';
-    angular.extend(this, {$log, ShiftService, DAY_OF_WEEK});
+    angular.extend(this, {$log, ShiftService, TimeslotService, LoadingService, DAY_OF_WEEK});
 
-    this.shifts = [{
-      dayOfWeek: 'MONDAY',
-      startTimeslot: 16,
-      endTimeslot: 34,
-      numEmployees: 2,
-      maxEmployees: 2,
-      admin: 0
-    },
-    {
-      dayOfWeek: 'MONDAY',
-      startTimeslot: 16,
-      endTimeslot: 34,
-      numEmployees: 1,
-      maxEmployees: 1,
-      admin: 1
-    },
-    {
-      dayOfWeek: 'TUESDAY',
-      startTimeslot: 16,
-      endTimeslot: 34,
-      numEmployees: 2,
-      maxEmployees: 2,
-      admin: 0
-    },
-    {
-      dayOfWeek: 'WEDNESDAY',
-      startTimeslot: 16,
-      endTimeslot: 34,
-      numEmployees: 2,
-      maxEmployees: 2,
-      admin: 0
-    },
-    {
-      dayOfWeek: 'THURSDAY',
-      startTimeslot: 16,
-      endTimeslot: 34,
-      numEmployees: 2,
-      maxEmployees: 2,
-      admin: 0
-    },
-    {
-      dayOfWeek: 'FRIDAY',
-      startTimeslot: 16,
-      endTimeslot: 34,
-      numEmployees: 2,
-      maxEmployees: 2,
-      admin: 0
-    }];
-    this.groupedShifts = this.ShiftService.groupShiftsByDay(this.shifts);
-    this.newShift = {};
+    this.LoadingService.loading = true;
+    this.ShiftService.getAllShifts()
+      .then(response => {
+        this.LoadingService.loading = false;
+        this.shifts = response.data;
+        this.groupedShifts = this.ShiftService.groupShiftsByDay(this.shifts);
+        this.newShift = {};
+      })
+      .catch(error => {
+        this.LoadingService.loading = false;
+        let message;
+        if (error.data && error.data.message) {
+          message = error.data.message;
+        } else {
+          message = 'An unknown error occurred';
+        }
+        this.AlertHandler.error(message);
+      });
   }
 
   addShift(dow) {
